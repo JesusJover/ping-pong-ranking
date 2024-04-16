58a13d8a44e7cb27cf8777f5f21545a4193cc682
Jesús Jover <jesus.jover@alu.uclm.es>
2024-04-16 14:31:23 +0200

    Refactor LastMatches component to include player names and points in the UI

--- app/components/LastMatches.jsx
+++ app/components/LastMatches.jsx
@@ -1,8 +1,39 @@
 import db from '../models/_connection-admin'
+import { firestore } from 'firebase-admin'
 
 export default async function LastMatches() {
-   const { docs } = await db.collection('partidos').orderBy('fin', 'desc').limit(5).get()
-   const matches = docs.map(doc => doc.data())
+   const { docs: matchesDocs } = await db.collection('partidos')
+                           .orderBy('fin', 'desc')
+                           .limit(5)
+                           .get()
+   let matches = matchesDocs.map(doc => doc.data())
+
+   const playersIds = []
+   matches.map(match => {
+      if (!playersIds.includes(match.jugador1)) {
+         playersIds.push(match.jugador1)
+      }
+      if (!playersIds.includes(match.jugador2)) {
+         playersIds.push(match.jugador2)
+      }
+   })
+
+   const { docs: playersDocs} = await db.collection('jugadores')
+                           .where("id", 'in', playersIds)
+                           .get()
+
+   const players = playersDocs.map(doc => doc.data())
+
+   matches = matches.map(match => {
+      const player1 = players.find(player => player.id === match.jugador1)
+      const player2 = players.find(player => player.id === match.jugador2)
+
+      return {
+         ...match,
+         jugador1: player1,
+         jugador2: player2
+      }
+   })
 
    return (
       <div className="w-full h-full flex flex-col lg:justify-center items-center gap-5">
@@ -12,8 +43,8 @@ export default async function LastMatches() {
                <div key={index} className="p-3 odd:bg-slate-200 even:bg-white
                   grid grid-cols-3 justify-between">
                   <div className={`flex-col items-start ${match.puntuacion1 > match.puntuacion2 ? "text-green-700" : "text-red-500"}`}>
-                     <h3 className="text-xl font-bold">{match.jugador1}</h3>
-                     <p>{match.puntos1}</p>
+                     <h3 className="text-xl font-bold">{match.jugador1.nombre}</h3>
+                     <p>{match.puntos1 > 0 ? `+${match.puntos1}` : match.puntos1}</p>
                   </div>
 
                   <div className="flex justify-center items-center gap-2">
@@ -23,8 +54,8 @@ export default async function LastMatches() {
                   </div>
 
                  <div className={`flex-col items-end ${match.puntuacion1 < match.puntuacion2 ? "text-green-700" : "text-red-500"}`}>
-                     <h3 className="text-xl font-bold text-right">{match.jugador2}</h3>
-                     <p className='text-right'>{match.puntos2}</p>
+                     <h3 className="text-xl font-bold text-right">{match.jugador2.nombre}</h3>
+                     <p className='text-right'>{match.puntos2 > 0 ? `+${match.puntos2}` : match.puntos2}</p>
                  </div>
                </div>
                )
